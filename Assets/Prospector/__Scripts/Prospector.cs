5a25349089fead72f5b81bbc6c4b25f34b8a4df8
d00dledan <djc382@scarletmail.rutgers.edu>
2022-10-27 18:44:51 -0400

    Implement CardProspector Script

--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 
 
 public class Prospector : MonoBehaviour {
@@ -17,6 +18,7 @@ public class Prospector : MonoBehaviour {
 	[Header("Set Dynamically")]
 	public Deck					deck;
 	public Layout layout;
+	public List<CardProspector> drawPile;
 
 	void Awake(){
 		S = this;
@@ -27,6 +29,20 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
 		layout = GetComponent<Layout> ();
 		layout.ReadLayout(layoutXML.text);
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+	}
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;                                     // a 
+            lCP.Add(tCP);
+        }
+        return (lCP);
 	}
 
 }
